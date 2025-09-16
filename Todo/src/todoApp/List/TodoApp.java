152ddf1b042b5c693b63871ba738b761b5402390
dlwkdgur <145451730+dlwkdgur@users.noreply.github.com>
2025-09-16 23:17:29 +0900

    2025.09.16

--- Todo/src/todoApp/List/TodoApp.java
+++ Todo/src/todoApp/List/TodoApp.java
@@ -3,8 +3,6 @@ package todoApp.List;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class TodoApp {
 	public static void main(String[] args) throws IOException {
